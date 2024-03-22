d598168216441b4bf4293339a64de57841d82bf6
chin <2294782638@qq.com>
2024-03-23 02:10:08 +0800

    just do the assignment

---
+++ src/abstractTest/train.java
@@ -0,0 +1,15 @@
+package abstractTest;
+
+/**
+ * @author Chin
+ * @description
+ * @date 2023/10/15 4:49 下午
+ **/
+public class train {
+    public static void main(String[] args) {
+       /* Student student = new Student();
+        student.doing();
+        student.teach();*/
+
+    }
+}
